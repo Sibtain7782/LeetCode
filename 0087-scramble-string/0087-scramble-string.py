508b02be4eeda497d5c526166074698cbadfbea6
MOHAMMAD SIBTAIN <sibtainraza59684@gmail.com>
2026-06-29 23:22:25 +0530

    Time: 11 ms (76.43%), Space: 21 MB (49.32%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.py
@@ -0,0 +1,30 @@
+from functools import lru_cache
+
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        
+        @lru_cache(None)
+        def dfs(a, b):
+            # Same strings
+            if a == b:
+                return True
+
+            # Different character counts
+            if sorted(a) != sorted(b):
+                return False
+
+            n = len(a)
+
+            # Try every split
+            for i in range(1, n):
+                # Case 1: No swap
+                if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
+                    return True
+
+                # Case 2: Swap
+                if dfs(a[:i], b[n-i:]) and dfs(a[i:], b[:n-i]):
+                    return True
+
+            return False
+
+        return dfs(s1, s2)
